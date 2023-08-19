07796aa16310ef4e6d8267daea1ed80e65a4c687
julio <juliocesar1316@gmail.com>
2023-08-19 20:27:50 -0300

    inciando projeto spring no github

---
+++ src/main/java/br/gov/sp/fatec/projectspringbd4/ProjectSpringBd4Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projectspringbd4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBd4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBd4Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/projectspringbd4/ProjectSpringBd4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projectspringbd4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBd4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
